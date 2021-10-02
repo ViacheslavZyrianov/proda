5a2133e1044a1ae0be9b3e003852831e9dbd6f3c
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2021-10-03 02:29:07 +0300

    add 'vite-plugin-legacy'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import legacyPlugin from 'vite-plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({})
+    VitePWA({}),
+    legacyPlugin({
+      targets: [
+        '> 0.5%',
+        'last 2 versions',
+        'Firefox ESR',
+        'not dead',
+      ],
+    })
   ],
   build: {
     target: 'esnext',
