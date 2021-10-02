adb86cac90ebd5055f11ed99433ad0bd744f2355
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2021-10-03 01:26:54 +0300

    add pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
   build: {
     target: 'esnext',
     minify: 'esbuild'
-  }
+  },
+  plugins: [
+    VitePWA({})
+  ]
 })
