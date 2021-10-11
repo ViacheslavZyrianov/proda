34248c0a8614ee94d6a5ca36bb7a322d68c43141
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2021-10-11 18:10:06 +0300

    add button Add cost

--- src/views/Costs/Index.vue
+++ src/views/Costs/Index.vue
@@ -24,11 +24,52 @@ function onEditCost() {
 </script>
 
 <template>
-  <costs-table
-    @edit="onEditCost"
-  />
+  <section class="page_costs">
+    <div class="costs__header">
+      <h1 class="header__title">Costs</h1>
+      <el-button
+        type="success"
+        size="small"
+        @click="onAddCostClick"
+      >
+        <i class="el-icon-plus el-icon-left"></i>
+        <span class="button-text">Add cost</span>
+      </el-button>
+    </div>
+    <costs-table
+      @edit="onEditCost"
+    />
+  </section>
   <add-edit-cost
     :is-visible="isAddEditCostVisible"
     @close="onAddEditCostClose"
   />
 </template>
+
+<style lang="scss">
+.page_costs {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+  .costs {
+    &__header {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+
+      .header {
+        &__title {
+          margin-right: auto;
+        }
+      }
+
+      .button-text {
+        @media screen and (max-width: 960px) {
+          display: none;
+        }
+      }
+    }
+  }
+}
+</style>
