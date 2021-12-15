b32ac34c1b9e7922d673120e2c1d223142362258
ViacheslavZyrianov <zyrianov.viacheslav@gmail.com>
2021-12-15 16:43:48 +0200

    add 30s timeout for requests

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 axios.defaults.headers['X-API-KEY'] = import.meta.env.VITE_TOKEN
 axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
+axios.defaults.timeout = 30000
